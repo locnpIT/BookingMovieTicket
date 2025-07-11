0c4231b0b5e481815334dd46cdd16701a5672327
Loc <loc@Locs-MacBook-Pro.local>
2025-07-11 09:23:22 +0700

    project init

---
+++ bookingmovieticket/src/main/java/com/example/phuocloc/bookingmovieticket/model/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.phuocloc.bookingmovieticket.model;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+    
+    
+
+}
---
+++ bookingmovieticket/src/test/java/com/example/phuocloc/bookingmovieticket/BookingmovieticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phuocloc.bookingmovieticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingmovieticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
